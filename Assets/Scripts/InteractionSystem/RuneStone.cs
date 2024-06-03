1137893efc9714481e4ab1ab7a1498241ebf2453
begumayten <79323462+begumayten@users.noreply.github.com>
2024-06-03 23:57:37 +0300

    interaction implemented

---
+++ Assets/Scripts/InteractionSystem/RuneStone.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RuneStone : MonoBehaviour, IInteractable
+{
+    [SerializeField] private string interactionPrompt;
+
+    public string InteractionPrompt => interactionPrompt;
+
+    public bool Interact(Interactor interactor, out bool interactWasSuccessful)
+    {
+
+        interactWasSuccessful = true;
+        Debug.Log("Interacted with the rune stone.");
+        return interactWasSuccessful;
+    }
+}
